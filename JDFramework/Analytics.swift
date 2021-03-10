e00d6e1b16b13413d408ef5a014241b8ef6b589b
jaydeep-inx <66677099+jaydeep-inx@users.noreply.github.com>
2021-03-10 14:19:57 +0530

    Added framework source and podspec

---
+++ JDFramework/Analytics.swift
@@ -0,0 +1,15 @@
+//
+//  Analytics.swift
+//  JDFramework
+//
+//  Created by Inx-MacMini-i3 on 10/03/21.
+//
+
+import Foundation
+public class Analytics {
+    public init() {}
+    
+    public func log(message: String) {
+        print("JD LOGS:: ", message)
+    }
+}

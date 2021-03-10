71d0ef09ee294910337f127e5cab4765c1aa3076
Jaydeep Vegad <jaydeep@inheritx.com>
2021-03-10 14:18:10 +0530

    Added framework source and podspec

---
+++ JDFramework/JDFramework.h
@@ -0,0 +1,18 @@
+//
+//  JDFramework.h
+//  JDFramework
+//
+//  Created by Inx-MacMini-i3 on 10/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JDFramework.
+FOUNDATION_EXPORT double JDFrameworkVersionNumber;
+
+//! Project version string for JDFramework.
+FOUNDATION_EXPORT const unsigned char JDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDFramework/PublicHeader.h>
+
+
